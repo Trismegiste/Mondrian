fd7b8915d5ef2e62ff6e99a80618b1870a0bb8be
Trismegiste <florent.g@allopneus.com>
2013-03-31 21:24:10 +0200

    simpler, faster and cooler

--- Transform/Format/Graphviz.php
+++ Transform/Format/Graphviz.php
@@ -16,23 +16,17 @@ use Trismegiste\Mondrian\Graph\Vertex;
 class Graphviz extends GraphExporter
 {
 
-    private function getIndex(Vertex $vertex)
-    {
-        preg_match('#([^\\\\]+)$#', get_class($vertex), $capt);
-        $type = $capt[1];
-        $filtered = preg_replace('#([^A-Za-z0-9])#', '_', $vertex->getName());
-        return $type . $filtered;
-    }
-
     public function export()
     {
         $default = array('fixedsize' => true, 'width' => 2, 'height' => 2);
         $dot = new Digraph('G');
-        foreach ($this->graph->getVertexSet() as $vertex) {
-            $dot->node($this->getIndex($vertex), array_merge($default, $vertex->getAttribute()));
+        $inverseIndex = new \SplObjectStorage();
+        foreach ($this->graph->getVertexSet() as $idx => $vertex) {
+            $inverseIndex[$vertex] = $idx;
+            $dot->node($idx, array_merge($default, $vertex->getAttribute()));
         }
         foreach ($this->graph->getEdgeSet() as $edge) {
-            $dot->edge(array($this->getIndex($edge->getSource()), $this->getIndex($edge->getTarget())));
+            $dot->edge(array($inverseIndex[$edge->getSource()], $inverseIndex[$edge->getTarget()]));
         }
         return $dot->render();
     }
