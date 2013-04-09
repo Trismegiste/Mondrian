99959c1b9a24fa8a3fd71fc2cea247784a7ad3d6
Florent <florent.g@allopneus.com>
2013-04-09 15:26:21 +0200

    CC for StaticAnalysis vertex

---
+++ Tests/Transform/Vertex/StaticAnalysisTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Mondrian
+ */
+
+namespace Trismegiste\Mondrian\Tests\Transform\Vertex;
+
+/**
+ * StaticAnalysisTest is a ...
+ *
+ * @author florent
+ */
+class StaticAnalysisTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $vertex;
+
+    protected function setUp()
+    {
+        $this->vertex = $this
+                ->getMockForAbstractClass('Trismegiste\Mondrian\Transform\Vertex\StaticAnalysis', array('a'));
+        $this->vertex->expects($this->any())
+                ->method('getSpecific')
+                ->will($this->returnValue(array()));
+    }
+
+    public function testAttribute()
+    {
+        $this->assertEquals(array(), $this->vertex->getAttribute());
+    }
+
+    public function testCentralityMeta()
+    {
+        $this->vertex->setMeta('centrality', 5);
+        $this->assertArrayHasKey('color', $this->vertex->getAttribute());
+    }
+
+}
