0ad493f66190c0f6408189cc5337a5fc2af1274a
Trismegiste <florent.g@allopneus.com>
2013-04-13 23:38:04 +0200

    dry or die

--- Command/CyclicCommand.php
+++ Command/CyclicCommand.php
@@ -8,7 +8,7 @@ namespace Trismegiste\Mondrian\Command;
 
 use Symfony\Component\Console\Output\OutputInterface;
 use Trismegiste\Mondrian\Graph\Graph;
-use Trismegiste\Mondrian\Graph\Tarjan;
+use Trismegiste\Mondrian\Analysis\Cycle;
 
 /**
  * CyclicCommand reduces a graph to its strongly connected components
@@ -28,33 +28,7 @@ class CyclicCommand extends AbstractParse
 
     protected function processGraph(Graph $graph, OutputInterface $output)
     {
-        $default = array('fixedsize' => true, 'width' => 2, 'height' => 2);
-        $dot = new \Alom\Graphviz\Digraph('G');
-
-        $inverseIndex = new \SplObjectStorage();
-        foreach ($graph->getVertexSet() as $idx => $vertex) {
-            $inverseIndex[$vertex] = $idx;
-            $dot->node($idx, array_merge($default, $vertex->getAttribute()));
-        }
-        foreach ($graph->getEdgeSet() as $edge) {
-            $dot->edge(array($inverseIndex[$edge->getSource()], $inverseIndex[$edge->getTarget()]));
-        }
-
-        $algo = new Tarjan($graph);
-        foreach ($algo->getStronglyConnected() as $idx => $subgraph) {
-            if (count($subgraph) > 2) {
-                $scc = $dot->subgraph("cluster_$idx");
-                $scc->attr('graph', array('bgcolor' => 'antiquewhite'));
-                foreach ($subgraph as $vertex) {
-                    $scc->node($inverseIndex[$vertex]);
-                }
-                $scc->end();
-            }
-        }
-
-        file_put_contents("wesh.dot", $dot->render());
-
-        return null;
+        return new Cycle($graph);
     }
 
 }
