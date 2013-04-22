80513e836e4287d68a4957d12b4fd5556245fcd5
Trismegiste <florent.g@allopneus.com>
2013-04-22 22:42:12 +0200

    DIP

--- Parser/PackageParser.php
+++ Parser/PackageParser.php
@@ -8,7 +8,6 @@ namespace Trismegiste\Mondrian\Parser;
 
 use Symfony\Component\Finder\SplFileInfo;
 use PHPParser_Parser;
-use PHPParser_Lexer;
 
 /**
  * PackageParser is a parser for multiple files
@@ -16,21 +15,26 @@ use PHPParser_Lexer;
 class PackageParser
 {
 
+    protected $fileParser;
+
+    public function __construct(PHPParser_Parser $parser)
+    {
+        $this->fileParser = $parser;
+    }
+
     public function parse(\Iterator $iter)
     {
-        $parser = new PHPParser_Parser(new PHPParser_Lexer());
-
         $node = array();
         foreach ($iter as $fch) {
-            $node[] = $this->createPhpFileNode($parser, $fch);
+            $node[] = $this->createPhpFileNode($fch);
         }
 
         return $node;
     }
 
-    protected function createPhpFileNode(PHPParser_Parser $parser, SplFileInfo $fch)
+    protected function createPhpFileNode(SplFileInfo $fch)
     {
-        return new PphFile($fch->getRealPath(), $parser->parse($fch->getContents()));
+        return new PphFile($fch->getRealPath(), $this->fileParser->parse($fch->getContents()));
     }
 
 }
