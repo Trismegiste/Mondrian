53a5d67756ddd93070c5a89d2b26236de5cf9865
Florent <florent.g@allopneus.com>
2013-04-25 17:24:56 +0200

    adding tests for Context

---
+++ Tests/Transform/TypeContextTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Mondrian
+ */
+
+namespace Trismegiste\Mondrian\Tests\Transform;
+
+use Trismegiste\Mondrian\Transform\Context;
+
+/**
+ * TypeContextTest tests for Context
+ */
+class TypeContextTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $context;
+
+    protected function setUp()
+    {
+        $this->context = new Context();
+    }
+
+    public function testEmpty()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testInitClass()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testInitInterface()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
---
+++ Tests/Transform/VertexContextTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Mondrian
+ */
+
+namespace Trismegiste\Mondrian\Tests\Transform;
+
+use Trismegiste\Mondrian\Transform\Context;
+
+/**
+ * ContextTest tests for Context
+ */
+class VertexContextTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $context;
+
+    protected function setUp()
+    {
+        $this->context = new Context();
+    }
+
+    public function testEmpty()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testFindVertex()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExistsVertex()
+    {
+        $this->markTestIncomplete();
+    }
+
+}
