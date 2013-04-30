b35e4f7b199c36661153ad99c60b2d4aebbcc47b
Trismegiste <florent.g@allopneus.com>
2013-05-01 01:14:37 +0200

    test for edge visitor : interface

--- Tests/Visitor/EdgeCollectorTest.php
+++ Tests/Visitor/EdgeCollectorTest.php
@@ -66,4 +66,43 @@ class EdgeCollectorTest extends \PHPUnit_Framework_TestCase
         $this->visitor->enterNode($classNode);
     }
 
+    public function testInterfaceInheritance()
+    {
+        $vertex = $this->getMockBuilder('Trismegiste\Mondrian\Graph\Vertex')
+                ->disableOriginalConstructor()
+                ->getMock();
+        $nsNode = new \PHPParser_Node_Stmt_Namespace(new \PHPParser_Node_Name('Atavachron'));
+        $classNode = new \PHPParser_Node_Stmt_Interface('Funnels');
+        $classNode->extends[] = new \PHPParser_Node_Name('Looking');
+        $classNode->extends[] = new \PHPParser_Node_Name('Glass');
+
+        $this->context
+                ->expects($this->exactly(3))
+                ->method('findVertex')
+                ->will($this->returnValue($vertex));
+
+        $this->context
+                ->expects($this->at(0))
+                ->method('findVertex')
+                ->with('interface', 'Atavachron\Funnels');
+
+        $this->context
+                ->expects($this->at(1))
+                ->method('findVertex')
+                ->with('interface', 'Atavachron\Looking');
+
+        $this->context
+                ->expects($this->at(2))
+                ->method('findVertex')
+                ->with('interface', 'Atavachron\Glass');
+
+        $this->graph
+                ->expects($this->exactly(2))
+                ->method('addEdge')
+                ->with($vertex, $vertex);
+
+        $this->visitor->enterNode($nsNode);
+        $this->visitor->enterNode($classNode);
+    }
+
 }
