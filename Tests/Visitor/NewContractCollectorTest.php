71cb2e19dbacc010761531cd6d9b95886d63da42
Florent <florent.g@allopneus.com>
2013-04-30 11:00:56 +0200

    testing for pushing new contract

--- Tests/Visitor/NewContractCollectorTest.php
+++ Tests/Visitor/NewContractCollectorTest.php
@@ -15,10 +15,85 @@ class NewContractCollectorTest extends \PHPUnit_Framework_TestCase
 {
 
     protected $visitor;
+    protected $context;
 
     protected function setUp()
     {
-        $this->visitor = new NewContractCollector();
+        $this->context = $this->getMockBuilder('Trismegiste\Mondrian\Refactor\Refactored')
+                ->getMock();
+        $this->visitor = new NewContractCollector($this->context);
+    }
+
+    protected function buildClassNode()
+    {
+        $node = $this->getMockBuilder('PHPParser_Node_Stmt_Class')
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $node->expects($this->once())
+                ->method('getType')
+                ->will($this->returnValue('Stmt_Class'));
+
+        return $node;
+    }
+
+    public function testInitialization()
+    {
+        $this->assertFalse($this->visitor->isModified());
+    }
+
+    public function testEnterClassWithoutComments()
+    {
+        $node = $this->buildClassNode();
+
+        $node->expects($this->exactly(2))
+                ->method('hasAttribute')
+                ->will($this->returnValueMap(array(
+                            array('comments', false),
+                            array('contractor', false)
+                        )));
+
+        $this->context->expects($this->never())
+                ->method('pushNewContract');
+
+        $this->visitor->enterNode($node);
+    }
+
+    public function testEnterCommentedClassWithoutAnnotations()
+    {
+        $node = $this->buildClassNode();
+
+        $node->expects($this->exactly(2))
+                ->method('hasAttribute')
+                ->will($this->returnValueMap(array(
+                            array('comments', true),
+                            array('contractor', false)
+                        )));
+
+        $node->expects($this->once())
+                ->method('getAttribute')
+                ->with('comments')
+                ->will($this->returnValue(array(new \PHPParser_Comment('Some useless comments'))));
+
+        $this->context->expects($this->never())
+                ->method('pushNewContract');
+
+        $this->visitor->enterNode($node);
+    }
+
+    public function testEnterAnnotedClass()
+    {
+        $node = new \PHPParser_Node_Stmt_Class('Glass', array(), array(
+                    'comments' => array(
+                        new \PHPParser_Comment('@mondrian contractor SomeNewContract')
+                    )
+                ));
+
+        $this->context->expects($this->once())
+                ->method('pushNewContract')
+                ->with('Glass', 'SomeNewContract');
+
+        $this->visitor->enterNode($node);
     }
 
 }
