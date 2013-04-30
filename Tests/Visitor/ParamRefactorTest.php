dee2f7888fc58fdbad76d79031431f702864021d
Florent <florent.g@allopneus.com>
2013-04-30 11:27:44 +0200

    tests for standard cases

--- Tests/Visitor/ParamRefactorTest.php
+++ Tests/Visitor/ParamRefactorTest.php
@@ -24,4 +24,42 @@ class ParamRefactorTest extends \PHPUnit_Framework_TestCase
         $this->visitor = new ParamRefactor($this->context);
     }
 
+    public function testNonTypedParam()
+    {
+        $node = new \PHPParser_Node_Param('obj');
+        $this->context->expects($this->never())
+                ->method('hasNewContract');
+        $this->visitor->enterNode($node);
+    }
+
+    public function testTypedParam()
+    {
+        $node = new \PHPParser_Node_Param('obj', null, 'array');
+        $this->context->expects($this->never())
+                ->method('hasNewContract');
+        $this->visitor->enterNode($node);
+    }
+
+    public function testClassTypedParamWithName()
+    {
+        $node = new \PHPParser_Node_Param('obj', null, new \PHPParser_Node_Name('SplObjectStorage'));
+        $this->context->expects($this->once())
+                ->method('hasNewContract')
+                ->with('SplObjectStorage')
+                ->will($this->returnValue(true));
+
+        $this->visitor->enterNode($node);
+    }
+
+    public function testClassTypedParamWithFqcn()
+    {
+        $node = new \PHPParser_Node_Param('obj', null, new \PHPParser_Node_Name_FullyQualified('Pull\Me\Under'));
+        $this->context->expects($this->once())
+                ->method('hasNewContract')
+                ->with('Pull\Me\Under')
+                ->will($this->returnValue(true));
+
+        $this->visitor->enterNode($node);
+    }
+
 }
