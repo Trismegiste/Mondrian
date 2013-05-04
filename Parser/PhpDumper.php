19c40337180983a35fa22467b4d78d5b04dcdc7b
Trismegiste <florent.g@allopneus.com>
2013-05-04 21:53:54 +0200

    add a php dumper

---
+++ Parser/PhpDumper.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Mondrian
+ */
+
+namespace Trismegiste\Mondrian\Parser;
+
+/**
+ * PhpDumper dumps a PhpFile into a file
+ */
+class PhpDumper
+{
+
+    protected $prettyPrinter;
+
+    public function __construct()
+    {
+        $this->prettyPrinter = new \PHPParser_PrettyPrinter_Default();
+    }
+
+    public function write(PhpFile $aFile)
+    {
+        file_put_contents($aFile->getRealPath(), "<?php\n\n"
+                . $this->prettyPrinter->prettyPrint(iterator_to_array($aFile->getIterator())));
+    }
+
+}
