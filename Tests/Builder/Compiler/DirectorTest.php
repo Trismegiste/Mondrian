c5bf8dcc39b3447a7f709c7d44e40ab6ecdd152a
Florent <florent.g@allopneus.com>
2013-05-20 16:41:34 +0200

    add a test for compiler director

---
+++ Tests/Builder/Compiler/DirectorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Mondrian
+ */
+
+namespace Trismegiste\Mondrian\Tests\Builder\Compiler;
+
+use Trismegiste\Mondrian\Builder\Compiler\Director;
+
+/**
+ * DirectorTest tests the director that builds the Compiler with the help of the builder
+ */
+class DirectorTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $director;
+    protected $builder;
+
+    protected function setUp()
+    {
+        $this->builder = $this->getMock('Trismegiste\Mondrian\Builder\Compiler\BuilderInterface');
+        $this->director = new Director($this->builder);
+    }
+
+    public function testBuilding()
+    {
+        $this->builder
+                ->expects($this->once())
+                ->method('buildContext');
+        $this->builder
+                ->expects($this->once())
+                ->method('buildCollectors')
+                ->will($this->returnValue(array($this->getMock('Trismegiste\Mondrian\Visitor\FqcnHelper'))));
+        $this->builder
+                ->expects($this->once())
+                ->method('buildTraverser')
+                ->will($this->returnValue($this->getMock('PHPParser_NodeTraverser')));
+
+        $this->director->compile(array());
+    }
+
+}
