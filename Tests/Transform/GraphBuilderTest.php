c531393c9e3977462abcde00b35294d72007a30f
Trismegiste <florent.g@allopneus.com>
2013-05-23 23:09:52 +0200

    test building of graph with graphbuilder alone

--- Tests/Transform/GraphBuilderTest.php
+++ Tests/Transform/GraphBuilderTest.php
@@ -30,26 +30,94 @@ class GraphBuilderTest extends \PHPUnit_Framework_TestCase
         $this->director = new Director($this->builder);
     }
 
-    public function testParsing()
+    public function testOneBuilding()
     {
         $fac = new BuilderFactory();
-        $file = $fac->file('wesh.php')
+        $file = $fac->file('sample.php')
                         ->ns('Kitty')
-                        ->addUse('Little\Ball\Fur')
                         ->declaring(
                                 $fac->class('Soft')
                                 ->implement('Warm')
                                 ->addStmt(
                                         $fac
-                                        ->method('hello')
+                                        ->method('pur')
                                 )
                         )->getNode();
 
-        $stmt = iterator_to_array($file->getIterator());
-        $pp = new \PHPParser_PrettyPrinter_Default();
-        echo $pp->prettyPrint($stmt);
+        $this->graph->expects($this->at(0))
+                ->method('addVertex')
+                ->with($this->vertexConstraint('class', 'Kitty\Soft'));
+        $this->graph->expects($this->at(1))
+                ->method('addVertex')
+                ->with($this->vertexConstraint('method', 'Kitty\Soft::pur'));
+        $this->graph->expects($this->at(2))
+                ->method('addVertex')
+                ->with($this->vertexConstraint('impl', 'Kitty\Soft::pur')
+        );
+        $this->graph->expects($this->exactly(3))
+                ->method('addEdge');
 
         $this->director->compile(array($file));
+    }
+
+    public function testConcreteInheritance()
+    {
+        $fac = new BuilderFactory();
+        $package[0] = $fac->file('sample1.php')
+                        ->ns('Kitty')
+                        ->declaring(
+                                $fac->class('Soft')
+                                ->extend('Warm')
+                                ->addStmt(
+                                        $fac
+                                        ->method('pur')
+                                )
+                        )->getNode();
+
+        $package[1] = $fac->file('sample2.php')
+                        ->ns('Kitty')
+                        ->declaring(
+                                $fac->class('Warm')
+                                ->addStmt(
+                                        $fac
+                                        ->method('pur')
+                                )
+                        )->getNode();
+
+        $this->graph->expects($this->exactly(5))
+                ->method('addVertex');
+
+        $this->graph->expects($this->at(1))
+                ->method('addVertex')
+                ->with($this->vertexConstraint('impl', 'Kitty\Soft::pur'));
+
+        $this->graph->expects($this->at(4))
+                ->method('addVertex')
+                ->with($this->vertexConstraint('impl', 'Kitty\Warm::pur'));
+
+        $this->graph->expects($this->exactly(6))
+                ->method('addEdge');
+
+        $this->graph->expects($this->at(5))
+                ->method('addEdge')
+                ->with($this->vertexConstraint('class', 'Kitty\Soft'), $this->vertexConstraint('class', 'Kitty\Warm'));
+
+        $this->graph->expects($this->at(6))
+                ->method('addEdge')
+                ->with($this->vertexConstraint('impl', 'Kitty\Soft::pur'), $this->vertexConstraint('class', 'Kitty\Soft'));
+
+        $this->graph->expects($this->at(7))
+                ->method('addEdge')
+                ->with($this->vertexConstraint('class', 'Kitty\Soft'), $this->vertexConstraint('impl', 'Kitty\Soft::pur'));
+
+        $this->director->compile($package);
+    }
+
+    protected function vertexConstraint($type, $name)
+    {
+        return $this->logicalAnd($this
+                                ->isInstanceOf('Trismegiste\Mondrian\Transform\Vertex\\' . ucfirst($type) . 'Vertex'), $this
+                                ->attributeEqualTo('name', $name));
     }
 
 }
