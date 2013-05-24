b580b3aa1e09bd2e613f9f5feec4fc3b8cf94d2a
Florent <florent.g@allopneus.com>
2013-05-24 14:23:20 +0200

    refactor assertions test

--- Tests/Transform/GraphBuilderTest.php
+++ Tests/Transform/GraphBuilderTest.php
@@ -38,22 +38,12 @@ class GraphBuilderTest extends \PHPUnit_Framework_TestCase
                         ->declaring(
                                 $fac->class('Soft')
                                 ->implement('Warm')
-                                ->addStmt(
-                                        $fac
-                                        ->method('pur')
-                                )
+                                ->addStmt($fac->method('purr'))
                         )->getNode();
 
-        $this->graph->expects($this->at(0))
-                ->method('addVertex')
-                ->with($this->vertexConstraint('class', 'Kitty\Soft'));
-        $this->graph->expects($this->at(1))
-                ->method('addVertex')
-                ->with($this->vertexConstraint('method', 'Kitty\Soft::pur'));
-        $this->graph->expects($this->at(2))
-                ->method('addVertex')
-                ->with($this->vertexConstraint('impl', 'Kitty\Soft::pur')
-        );
+        $this->expectsAddVertex(0, 'class', 'Kitty\Soft');
+        $this->expectsAddVertex(1, 'method', 'Kitty\Soft::purr');
+        $this->expectsAddVertex(2, 'impl', 'Kitty\Soft::purr');
         $this->graph->expects($this->exactly(3))
                 ->method('addEdge');
 
@@ -68,33 +58,21 @@ class GraphBuilderTest extends \PHPUnit_Framework_TestCase
                         ->declaring(
                                 $fac->class('Soft')
                                 ->extend('Warm')
-                                ->addStmt(
-                                        $fac
-                                        ->method('pur')
-                                )
+                                ->addStmt($fac->method('purr'))
                         )->getNode();
 
         $package[1] = $fac->file('sample2.php')
                         ->ns('Kitty')
                         ->declaring(
                                 $fac->class('Warm')
-                                ->addStmt(
-                                        $fac
-                                        ->method('pur')
-                                )
+                                ->addStmt($fac->method('purr'))
                         )->getNode();
 
         $this->graph->expects($this->exactly(5))
                 ->method('addVertex');
 
-        $this->graph->expects($this->at(1))
-                ->method('addVertex')
-                ->with($this->vertexConstraint('impl', 'Kitty\Soft::pur'));
-
-        $this->graph->expects($this->at(4))
-                ->method('addVertex')
-                ->with($this->vertexConstraint('impl', 'Kitty\Warm::pur'));
-
+        $this->expectsAddVertex(1, 'impl', 'Kitty\Soft::purr');
+        $this->expectsAddVertex(4, 'impl', 'Kitty\Warm::purr');
         $this->graph->expects($this->exactly(6))
                 ->method('addEdge');
 
@@ -104,11 +82,11 @@ class GraphBuilderTest extends \PHPUnit_Framework_TestCase
 
         $this->graph->expects($this->at(6))
                 ->method('addEdge')
-                ->with($this->vertexConstraint('impl', 'Kitty\Soft::pur'), $this->vertexConstraint('class', 'Kitty\Soft'));
+                ->with($this->vertexConstraint('impl', 'Kitty\Soft::purr'), $this->vertexConstraint('class', 'Kitty\Soft'));
 
         $this->graph->expects($this->at(7))
                 ->method('addEdge')
-                ->with($this->vertexConstraint('class', 'Kitty\Soft'), $this->vertexConstraint('impl', 'Kitty\Soft::pur'));
+                ->with($this->vertexConstraint('class', 'Kitty\Soft'), $this->vertexConstraint('impl', 'Kitty\Soft::purr'));
 
         $this->director->compile($package);
     }
@@ -118,6 +96,13 @@ class GraphBuilderTest extends \PHPUnit_Framework_TestCase
         return $this->logicalAnd($this
                                 ->isInstanceOf('Trismegiste\Mondrian\Transform\Vertex\\' . ucfirst($type) . 'Vertex'), $this
                                 ->attributeEqualTo('name', $name));
+    }
+
+    protected function expectsAddVertex($idx, $type, $name)
+    {
+        $this->graph->expects($this->at($idx))
+                ->method('addVertex')
+                ->with($this->vertexConstraint($type, $name));
     }
 
 }
