4f869df2c8aacf785e58c417c0aeaaf958000a3b
Trismegiste <florent.g@allopneus.com>
2013-05-25 00:29:04 +0200

    valid config for Box

--- Resources/box/stub_for_box.php
+++ Resources/box/stub_for_box.php
@@ -7,7 +7,7 @@
 spl_autoload_register(function ($class) {
             if (preg_match('#^Trismegiste\\\\Mondrian\\\\(.+)$#', $class, $ret)) {
                 $relPath = str_replace('\\', DIRECTORY_SEPARATOR, $ret[1]);
-                require_once __DIR__ . DIRECTORY_SEPARATOR . $relPath . '.php';
+                require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . $relPath . '.php';
             }
         });
 
