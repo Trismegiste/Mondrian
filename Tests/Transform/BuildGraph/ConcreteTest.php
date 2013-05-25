4a35b7bfa0d08e227291221c8acf63e5a1f0cd3f
Trismegiste <florent.g@allopneus.com>
2013-05-25 13:43:07 +0200

    moving and splitting test case for graph building

--- Tests/Transform/BuildGraph/ConcreteTest.php
+++ Tests/Transform/BuildGraph/ConcreteTest.php
@@ -4,31 +4,15 @@
  * Mondrian
  */
 
-namespace Trismegiste\Mondrian\Tests\Transform;
+namespace Trismegiste\Mondrian\Tests\Transform\BuildGraph;
 
-use Trismegiste\Mondrian\Transform\GraphBuilder;
-use Trismegiste\Mondrian\Builder\Compiler\Director;
 use Trismegiste\Mondrian\Parser\BuilderFactory;
 
 /**
- * GraphBuilderTest tests the builder compiler
+ * ConcreteTest tests for building a graph on concrete class
  */
-class GraphBuilderTest extends \PHPUnit_Framework_TestCase
+class ConcreteTest extends GraphBuilderTestCase
 {
-
-    protected $builder;
-    protected $director;
-    protected $logger;
-    protected $graph;
-
-    protected function setUp()
-    {
-        $conf = array('calling' => array());
-        $this->graph = $this->getMock('Trismegiste\Mondrian\Graph\Graph');
-        $this->logger = $this->getMock('Trismegiste\Mondrian\Transform\Logger\LoggerInterface');
-        $this->builder = new GraphBuilder($conf, $this->graph, $this->logger);
-        $this->director = new Director($this->builder);
-    }
 
     public function testOneBuilding()
     {
@@ -78,27 +62,6 @@ class GraphBuilderTest extends \PHPUnit_Framework_TestCase
         $this->expectsAddEdge(7, 'class', 'Kitty\Soft', 'impl', 'Kitty\Soft::purr');
 
         $this->director->compile($package);
-    }
-
-    protected function vertexConstraint($type, $name)
-    {
-        return $this->logicalAnd($this
-                                ->isInstanceOf('Trismegiste\Mondrian\Transform\Vertex\\' . ucfirst($type) . 'Vertex'), $this
-                                ->attributeEqualTo('name', $name));
-    }
-
-    protected function expectsAddVertex($idx, $type, $name)
-    {
-        $this->graph->expects($this->at($idx))
-                ->method('addVertex')
-                ->with($this->vertexConstraint($type, $name));
-    }
-
-    protected function expectsAddEdge($idx, $type1, $name1, $type2, $name2)
-    {
-        $this->graph->expects($this->at($idx))
-                ->method('addEdge')
-                ->with($this->vertexConstraint($type1, $name1), $this->vertexConstraint($type2, $name2));
     }
 
 }
