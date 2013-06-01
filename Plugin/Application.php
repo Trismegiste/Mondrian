8b359745bdbd6201341de18586846b45427c8889
Trismegiste <florent.g@allopneus.com>
2013-06-01 23:42:18 +0200

    php 5.3.8 don't accept third parameter for is_subclass_of

--- Plugin/Application.php
+++ Plugin/Application.php
@@ -9,9 +9,7 @@ namespace Trismegiste\Mondrian\Plugin;
 use Symfony\Component\Console\Application as SymfoApp;
 
 /**
- * Application is a ...
- *
- * @author florent
+ * Application is an symfony app with plugin capabilities
  */
 class Application extends SymfoApp
 {
@@ -19,12 +17,12 @@ class Application extends SymfoApp
     /**
      * Adds a list of subclasses of Command
      *
-     * @param array $listing
+     * @param array $listing an array of fqcn symfony command
      */
     public function addPlugin(array $listing)
     {
         foreach ($listing as $cmd) {
-            if (is_subclass_of($cmd, 'Symfony\Component\Console\Command\Command', true)) {
+            if (is_subclass_of($cmd, 'Symfony\Component\Console\Command\Command')) {
                 $this->add(new $cmd());
             } else {
                 throw new \InvalidArgumentException("$cmd is not a Symfony Command");
