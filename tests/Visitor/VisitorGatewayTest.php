784e7f57731ed7e747bb3c2ee49a2aff49f90a82
Trismegiste <flo@trismegiste.net>
2014-09-16 02:27:31 +0200

    CC 100% for visitor gateway

--- tests/Visitor/VisitorGatewayTest.php
+++ tests/Visitor/VisitorGatewayTest.php
@@ -14,14 +14,32 @@ use Trismegiste\Mondrian\Visitor\VisitorGateway;
 class VisitorGatewayTest extends \PHPUnit_Framework_TestCase
 {
 
-    protected $sut;
     protected $reflectionCtx;
     protected $graphCtx;
     protected $graph;
 
+    /** @var VisitorGateway */
+    protected $sut;
+
     private function buildVisitor(array $visitor = [])
     {
         $this->sut = new VisitorGateway($visitor, $this->reflectionCtx, $this->graphCtx, $this->graph);
+    }
+
+    private function buildVisitorUnique()
+    {
+        $state = $this->getMockState('key');
+        $this->buildVisitor([$state]);
+    }
+
+    public function getMockState($key)
+    {
+        $state = $this->getMock('Trismegiste\Mondrian\Visitor\State\State');
+        $state->expects($this->any())
+                ->method('getName')
+                ->will($this->returnValue($key));
+
+        return $state;
     }
 
     protected function setUp()
@@ -66,6 +84,82 @@ class VisitorGatewayTest extends \PHPUnit_Framework_TestCase
                 ->method('setContext');
 
         $this->buildVisitor([$state0, $state1]);
+    }
+
+    public function testEnteringNode()
+    {
+        $state = $this->getMockState('key');
+        $state->expects($this->once())
+                ->method('enter');
+        $this->buildVisitor([$state]);
+        $node = $this->getMock('PhpParser\Node');
+        $this->sut->enterNode($node);
+    }
+
+    public function testGetState()
+    {
+        $this->buildVisitorUnique();
+        $this->assertInstanceOf('Trismegiste\Mondrian\Visitor\State\State', $this->sut->getState('key'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testGetUnknownState()
+    {
+        $this->buildVisitorUnique();
+        $this->sut->getState('svfdgbtgf');
+    }
+
+    public function testShortcut()
+    {
+        $this->buildVisitorUnique();
+        $this->sut->getGraph();
+        $this->sut->getGraphContext();
+        $this->sut->getReflectionContext();
+    }
+
+    public function testPushState()
+    {
+        $listing = [
+            $this->getMockState('one'),
+            $this->getMockState('two'),
+            $this->getMockState('three')
+        ];
+        $this->buildVisitor($listing);
+        $node = [
+            $this->getMock('PhpParser\Node'),
+            $this->getMock('PhpParser\Node'),
+            $this->getMock('PhpParser\Node'),
+        ];
+
+        $this->assertNull($this->sut->getNodeFor('one'));
+
+        $this->sut->pushState('two', $node[0]);
+        $this->assertNull($this->sut->getNodeFor('one'));
+        $this->assertEquals($node[0], $this->sut->getNodeFor('two'));
+
+        $this->sut->pushState('three', $node[1]);
+        $this->assertNull($this->sut->getNodeFor('one'));
+        $this->assertEquals($node[0], $this->sut->getNodeFor('two'));
+        $this->assertEquals($node[1], $this->sut->getNodeFor('three'));
+
+        $this->sut->leaveNode($node[1]);
+        $this->assertNull($this->sut->getNodeFor('one'));
+        $this->assertEquals($node[0], $this->sut->getNodeFor('two'));
+        $this->assertAttributeCount(2, 'stateStack', $this->sut);
+
+        $this->sut->leaveNode($node[0]);
+        $this->assertAttributeCount(1, 'stateStack', $this->sut);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testNoNodeFound()
+    {
+        $this->buildVisitorUnique();
+        $this->sut->getNodeFor('fddbfgb');
     }
 
 }
